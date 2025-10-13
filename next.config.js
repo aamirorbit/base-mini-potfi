6c1f438b474e06a17c6c102f7d5366437e5b3ba2
aamirorbit <aamirorbit@gmail.com>
2025-10-13 22:00:51 +0800

    feat: header fix there

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
               "img-src 'self' data: https: blob:",
               "font-src 'self' data:",
               "connect-src 'self' https://mainnet.base.org https://base.org https://explorer-api.walletconnect.com https://supercast.mypinata.cloud https://api.neynar.com https://*.walletconnect.com https://*.walletconnect.org wss://*.walletconnect.com wss://*.walletconnect.org",
-              "frame-ancestors 'self' https://warpcast.com https://*.warpcast.com https://farcaster.xyz https://*.farcaster.xyz https://client.farcaster.xyz",
+              "frame-ancestors 'self' https://warpcast.com https://*.warpcast.com https://farcaster.xyz https://*.farcaster.xyz https://client.farcaster.xyz https://*.vercel.app https://world.org https://*.world.org https://worldcoin.org https://*.worldcoin.org https://wallet.coinbase.com https://*.coinbase.com https://go.cb-w.com https://*.cb-w.com",
               "frame-src 'self' https://verify.walletconnect.com https://verify.walletconnect.org",
             ].join('; '),
           },
